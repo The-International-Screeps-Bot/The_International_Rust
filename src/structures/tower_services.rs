a44bda12e0a2704478854cf17699d27c357899df
Carson <carsonburke22@gmail.com>
2024-03-05 22:52:32 -0800

    basic tower logic, disabled

--- src/structures/tower_services.rs
+++ src/structures/tower_services.rs
@@ -1,5 +1,21 @@
+use screeps::{find, ObjectId, Room, StructureTower};
+
+use crate::{room::room_ops::RoomOps, state::game::GameState};
+
 pub struct TowerServices;
 
 impl TowerServices {
-    
+    pub fn run_towers(room: &Room, game_state: &mut GameState) {
+        // let towers = RoomOps::structures(room, game_state).towers;
+        // let mut used_towers: Vec<ObjectId<StructureTower>> = Vec::new();
+
+        // let creeps = RoomOps::not_my_creeps(room, game_state).enemy;
+        // if creeps.len() == 0 {
+        //     return
+        // }
+
+        // for tower in towers {
+        //     tower.attack(creeps[0]);
+        // }
+    }
 }
