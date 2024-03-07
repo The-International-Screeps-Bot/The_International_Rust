14e7bcdc61df0532be48a9474e4191c47068b3cc
Carson <carsonburke22@gmail.com>
2024-03-07 12:10:21 -0800

    functional but efficient construction site management: acheived

--- src/international/construction_site_services.rs
+++ src/international/construction_site_services.rs
@@ -1,4 +1,6 @@
-use screeps::{game, ConstructionSite};
+use std::collections::{HashMap, HashSet};
+
+use screeps::{game, ConstructionSite, MaybeHasId, ObjectId};
 
 use crate::{constants::general::{CONSTRUCTION_PROGRESS_AGE_MULTIPLIER, MIN_CONSTRUCTION_SITE_AGE}, memory::game_memory::GameMemory, utils::general::GeneralUtils};
 
@@ -10,40 +12,53 @@ impl ConstructionSiteServices {
     /// delete and remove sites that are too old
     /// increment the age of remaining sites
     pub fn manage_sites(memory: &mut GameMemory) {
+
+        // only run the following logic every so often
+
         let interval = 100;
         if !GeneralUtils::is_tick_interval(interval) {
             return
         }
 
-        let construction_sites = game::construction_sites();
+        /// Construction site ids that are known to exist
+        let mut construction_sites: HashMap<ObjectId<ConstructionSite>, ConstructionSite> = HashMap::new();
+        let js_construction_sites = game::construction_sites();
 
-        // FIXME: this runs into some borrow checker issues
-        for id in construction_sites.keys() {
-            if memory.construction_sites.contains_key(&id) {
-                continue
-            }
+        // Move the values from a js hashmap to the rust hashmap
 
-            memory.construction_sites.insert(id, 0);
-        }
-
-        for (id, age) in &memory.construction_sites {
-            let Some(site) = game::get_object_by_id_typed(id) else {
-
-                memory.construction_sites.remove(&id);
+        for  site in js_construction_sites.values() {
+            let Some(id) = site.try_id() else {
                 continue;
             };
+            construction_sites.insert(id, site);
+        }
 
-            if *age > Self::max_site_age(&site) {
+        // Remove caches for construction sites that no longer exist,
+        // delete and remove caches for construction sites that are too old relative to their progress
+
+        memory.construction_sites.retain(|id, age| {
+            let Some(site) = construction_sites.get(&id) else {
+
+                return false
+            };
+
+            if *age > Self::max_site_age(site.progress()) {
                 site.remove();
-                memory.construction_sites.remove(&id);
-                continue;
+                return false
             }
 
-            memory.construction_sites.insert(id.clone(), age + 1 * interval);
+            true
+        });
+        
+        // Increment the age of cached construction sites
+
+        for (id, age) in &mut memory.construction_sites {
+            // times inveral because we only run the code every interval, but we cant to track how many ticks have passed
+            *age += 1 * interval;
         }
     }
 
-    fn max_site_age(site: &ConstructionSite) -> u32 {
-        MIN_CONSTRUCTION_SITE_AGE + site.progress() * CONSTRUCTION_PROGRESS_AGE_MULTIPLIER
+    fn max_site_age(site_progress: u32) -> u32 {
+        MIN_CONSTRUCTION_SITE_AGE + site_progress * CONSTRUCTION_PROGRESS_AGE_MULTIPLIER
     }
 }
