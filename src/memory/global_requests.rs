17c49cea3fe9e404751ccb1b17ba25a0aaf0c4e2
Carson <carsonburke22@gmail.com>
2024-06-12 11:54:36 -0700

    Update global_requests.rs

--- src/memory/global_requests.rs
+++ src/memory/global_requests.rs
@@ -8,9 +8,19 @@ pub struct WorkRequest {
     /// May want to have multiple responders in the future
     pub responders: Option<HashSet<RoomName>>,
     /// how many persisent work parts are requested for the job
-    pub work_need: Option<u32>,
+    pub work_strength: Option<u32>,
     /// How many ticks to abandon the request for
     pub abandon: Option<u32>,
+}
+
+impl WorkRequest {
+    pub fn new() -> Self {
+        Self {
+            responders: None,
+            work_strength: None,
+            abandon: None,
+        }
+    }
 }
 
 pub type WorkRequests = HashMap<RoomName, WorkRequest>;
@@ -20,6 +30,17 @@ pub struct ClaimRequest {
     pub responder: Option<RoomName>,
     /// How many ticks to abandon the request for
     pub abandon: Option<u32>,
+    pub claimer: Option<bool>,
+}
+
+impl ClaimRequest {
+    pub fn new() -> Self {
+        Self {
+            responder: None,
+            abandon: None,
+            claimer: None,
+        }
+    }
 }
 
 pub type ClaimRequests = HashMap<RoomName, ClaimRequest>;
@@ -28,6 +49,15 @@ pub type ClaimRequests = HashMap<RoomName, ClaimRequest>;
 pub struct CombatRequest {
     pub responders: Option<RoomName>,
     pub abandon: Option<u32>,
+}
+
+impl CombatRequest {
+    pub fn new() -> Self {
+        Self {
+            responders: None,
+            abandon: None,
+        }
+    }
 }
 
 pub type CombatRequests = HashMap<RoomName, CombatRequest>;
