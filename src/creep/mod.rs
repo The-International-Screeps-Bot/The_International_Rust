b620df7d9f0619c28f1f46d7a8868ce0c32b2722
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2024-07-16 18:10:04 -0400

    add owned creep

--- src/creep/mod.rs
+++ src/creep/mod.rs
@@ -1,8 +1,9 @@
+pub mod any_creep_ops;
+pub mod creep_functions;
 pub mod creep_move_ops;
-pub mod my_creep_services;
 pub mod my_creep;
 pub mod my_creep_ops;
-pub mod roles;
-pub mod creep_functions;
+pub mod my_creep_services;
+pub mod owned_creep;
 pub mod role_services;
-pub mod any_creep_ops;
+pub mod roles;
